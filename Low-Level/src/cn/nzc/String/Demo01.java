35ab715d72de0d15412a02990cd63983856c9239
2516085027 <2516085027@qq.com>
2021-01-29 18:45:50 +0800

    nzc

--- Low-Level/src/cn/nzc/String/Demo01.java
+++ Low-Level/src/cn/nzc/String/Demo01.java
@@ -10,6 +10,32 @@
  */
 package cn.nzc.String;
 
-public class Demo01 {
+import java.util.Arrays;
 
+/**
+ * 反转字符串
+ * 编写一个函数，其作用是将输入的字符串反转过来。输入字符串以字符数组 char[] 的形式给出。
+ *
+ * 不要给另外的数组分配额外的空间，你必须原地修改输入数组、使用 O(1) 的额外空间解决这一问题。
+ *
+ * 你可以假设数组中的所有字符都是 ASCII 码表中的可打印字符。
+ *
+ * 作者：力扣 (LeetCode)
+ * 链接：https://leetcode-cn.com/leetbook/read/top-interview-questions-easy/xnhbqj/
+ * 来源：力扣（LeetCode）
+ * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+ */
+public class Demo01 {
+    public static void reverseString(char[] s) {
+        for (int i = 0; i < s.length / 2; i++) {
+            char temp = s[i];
+            s[i] = s[s.length - 1 - i];
+            s[s.length - 1 - i] = temp;
+        }
+    }
+    public static void main(String[] args) {
+        char[] s = {'h', 'e', 'l', 'l', 'o'};
+        reverseString(s);
+        System.out.println(Arrays.toString(s));
+    }
 }
---
+++ Low-Level/src/cn/nzc/String/Demo02.java
@@ -0,0 +1,33 @@
+/**
+ * Copyright (C), 2015-2021
+ * FileName: Demo02
+ * Author:   niko
+ * Date:     2021/1/29 15:13
+ * Description:
+ * History:
+ * <author>          <time>          <version>
+ * 逝不等琴生          15:13           1.0
+ */
+package cn.nzc.String;
+
+/**
+ * 整数反转
+ * 给你一个 32 位的有符号整数 x ，返回 x 中每位上的数字反转后的结果。
+ *
+ * 如果反转后整数超过 32 位的有符号整数的范围[−2^31, 2^31− 1] ，就返回 0。
+ *
+ * 假设环境不允许存储 64 位整数（有符号或无符号）。
+ *
+ * 作者：力扣 (LeetCode)
+ * 链接：https://leetcode-cn.com/leetbook/read/top-interview-questions-easy/xnx13t/
+ * 来源：力扣（LeetCode）
+ * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+ */
+public class Demo02 {
+    public static int reverse(int x) {
+
+    }
+    public static void main(String[] args) {
+
+    }
+}
